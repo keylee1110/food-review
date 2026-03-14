815f5092ac7560301246b14726438e43295103bf
khoald1110 <khoald1110@gmail.com>
2026-03-14 13:26:37 +0700

    fix: make sanity client build-safe and fallback to mock data

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
+  projectId: projectId || 'missing-project-id',
   dataset,
   apiVersion,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
